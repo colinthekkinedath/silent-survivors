1ee4df579bd37a5e77aada91bf7ef9cbd33964e8
colinthekkinedath <colinthekkinedath@gmail.com>
2023-01-15 11:07:46 -0800

    working???????

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -47,7 +47,6 @@ const CreatePost = () => {
                 Title
             </label>
             <textarea 
-                id="message" 
                 rows="`" 
                 className="resize-none p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
                 placeholder="Write your title here..."
@@ -62,7 +61,6 @@ const CreatePost = () => {
                 Your message
             </label>
             <textarea 
-                id="message" 
                 rows="4" 
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
                 placeholder="Write your thoughts here..."
