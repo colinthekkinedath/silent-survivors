10d7307e705bc749fd2d27b59b1a4e909a4f83c9
colinthekkinedath <colinthekkinedath@gmail.com>
2023-01-15 10:59:39 -0800

    about page completed

--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,69 @@
 import React from "react";
 
 const About = () => {
-  return(
-    <div>
-        About
-    </div>
-  ); 
+  return (
+    <div>
+      <style>
+        margin-top: 100px; margin-bottom: 100px; margin-right: 150px;
+        margin-left: 80px;
+      </style>
+      <center>
+        <h1>
+          <font size="8">
+            <b>Our Mission</b>
+          </font>
+        </h1>
+        <p>
+          According to NPR.org around 81% Of Women Have Experienced Sexual
+          Harassment, but 75% of cases go unreported. This clearly shows how
+          sexual harassment is prevalent within the lives of women. However,
+          when considering the fact that many women find these experiences
+          embarrassing or vulnerable, it leads to them simply burying the trauma
+          caused by such experiences. This is not the ideal way society must
+          deal with such actions. As trauma can wear down one’s mental health
+          and lead to negative long term effects such as depression.
+        </p>
+      </center>
+      <h2>
+        <b>In Fact</b>
+      </h2>
+      <ul>
+        <li>
+          94% of women who are raped experience symptoms of post-traumatic
+          stress disorder (PTSD) during the two weeks following the rape.
+        </li>
+        <li>30% of women report symptoms of PTSD 9 months after the rape.</li>
+        <li>33% of women who are raped contemplate suicide.</li>
+        <li>13% of women who are raped attempt suicide.</li>
+        <li>
+          Approximately 70% of rape or sexual assault victims experience
+          moderate to severe distress, a larger percentage than for any other
+          violent crime.
+        </li>
+        <li>
+          People who have been sexually assaulted are more likely to use drugs
+          than the general public.
+          <ul>
+            <li>3.4 times more likely to use marijuana</li>
+            <li>6 times more likely to use cocaine</li>
+            <li>10 times more likely to use other major drugs</li>
+          </ul>
+        </li>
+      </ul>
+      <p>
+        <center>
+          It is unacceptable that we live in a country where people are sexually
+          assaulted every 68 seconds. This is why we started the silent
+          survivors. As these cases grow, but the epidemic ceases to die, we
+          must bring greater awareness to the public. The silent survivors not
+          only allow survivors a safespace to vent out their trauma anonymously
+          to others, but it also conveys the problem at hand by displaying a
+          vivid picture of the severity of the problem as more cases are spoken
+          of.
+        </center>
+      </p>
+    </div>
+  );
 };
 
 export default About;
